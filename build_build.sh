f0e04e78a3fdff39a21c6389d53332ea4045b5e3
Colin Bellino <git+snowball2@colinbellino.com>
2023-08-03 21:10:41 +0200

    Fix windows build

--- build_build.sh
+++ build_build.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-odin build ./src/build.odin -file -out:build.exe -o=speed -extra-linker-flags:"-F src/sdl2"
+extra=""
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    extra="-extra-linker-flags:-F. -rpath @loader_path"
+fi
+
+odin build ./src/build.odin -file -out:build.exe -o=speed "$extra"
